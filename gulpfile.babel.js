c1f1afe03f0007aee33fbddfc7503c1b2b59fccf
Amie Wilt <amiewilt@AwesomeAmie.local>
2017-07-16 15:41:12 -0400

    Add prettier and format updates

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,39 +6,37 @@ import cleanCss from 'gulp-clean-css';
 const bs = require('browser-sync').create();
 
 gulp.task('styles', () => {
-    gulp.src('src/css/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({ browsers: ['last 3 versions', 'Safari 8'] }))
-        .pipe(sourcemaps.write())
-        .pipe(cleanCss())
-        .pipe(gulp.dest('build/css'))
-        .pipe(bs.reload({ stream: true }));
+  gulp
+    .src('src/css/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['last 3 versions', 'Safari 8'] }))
+    .pipe(sourcemaps.write())
+    .pipe(cleanCss())
+    .pipe(gulp.dest('build/css'))
+    .pipe(bs.reload({ stream: true }));
 });
 
 gulp.task('html', () => {
-    return gulp.src('src/*.html')
-        .pipe(gulp.dest('build'))
+  return gulp.src('src/*.html').pipe(gulp.dest('build'));
 });
 
 gulp.task('images', () => {
-    return gulp.src('src/images/**')
-        .pipe(gulp.dest('build/images'))
+  return gulp.src('src/images/**').pipe(gulp.dest('build/images'));
 });
 
-gulp.task('server', function () {
-    bs.init({
-        server: {
-            baseDir: "./build"
-        }
-    });
+gulp.task('server', function() {
+  bs.init({
+    server: {
+      baseDir: './build',
+    },
+  });
 });
 
 gulp.task('serve', ['server'], () => {
-    gulp.watch('src/css/*.scss', ['styles']);
-    gulp.watch('src/images/**', ['images']);
-    gulp.watch('src/*.html').on('change', bs.reload);
+  gulp.watch('src/css/*.scss', ['styles']);
+  gulp.watch('src/images/**', ['images']);
+  gulp.watch('src/*.html').on('change', bs.reload);
 });
 
 gulp.task('build', ['html', 'styles', 'images']);
-
